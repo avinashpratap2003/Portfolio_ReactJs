870d236f14437e43d4397af6b3ab521f298d08f0
avinashpratap2003 <pratapavinash843@gmail.com>
2024-04-29 04:14:27 +0530

    uploaded first project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,26 +13,26 @@ const Projects = () => {
             <div className="p-number p-n-right">01</div>
             <div className="card-content ">
               <div className="card-content-skills">
-                <img className="icon" src="stacks/HTML.png" alt="" />
-                <img className="icon" src="stacks/CSS.png" alt="" />
-                <img className="icon" src="stacks/Javascript.svg" alt="" />
-                <img className="icon" src="stacks/Express.png" alt="" />
-                <img className="icon" src="stacks/Next.svg" alt="" />
-                <img className="icon" src="stacks/Tailwind.png" alt="" />
-                <img className="icon" src="stacks/NodeJs.svg" alt="" />
-                <img className="icon" src="stacks/MongoDB.svg" alt="" />
-                <img className="icon" src="stacks/Redux.svg" alt="" />
-                <img className="icon" src="stacks/Vercel.svg" alt="" />
+                <img className="icon" src="stacks/HTML.png" alt="html" />
+                <img className="icon" src="stacks/CSS.png" alt="css" />
+                <img className="icon" src="stacks/Javascript.svg" alt="java" />
+                {/* <img className="icon" src="stacks/Express.png" alt="" /> */}
+                {/* <img className="icon" src="stacks/Next.svg" alt="" /> */}
+                {/* <img className="icon" src="stacks/Tailwind.png" alt="" /> */}
+                {/* <img className="icon" src="stacks/NodeJs.svg" alt="" /> */}
+                {/* <img className="icon" src="stacks/MongoDB.svg" alt="" /> */}
+                <img className="icon" src="stacks/React.png" alt="reactjs" />
+                {/* <img className="icon" src="stacks/Vercel.svg" alt="" /> */}
               </div>
-              <h2 className="tint-n-orange">Tint & Orange</h2>
-              <p className="paragraph">
-                Its is a car modification copany which provide you sheets to
-                protect your car from scratch
+              <h2 className="project-name">Portfolio</h2>
+              <p className="project-description">
+                This website has been made using ReactJS and CSS. This Portfolio contains all my works.
               </p>
               <div className="btn-group">
-                <button className="button card-button ">Read More</button>
-                <a title="Github" href=""><FaGithub /></a>
-                <a title="Project Link" href=""><PiLinkSimpleBold /></a>
+                <button className="button card-button ">
+                  <a href="https://github.com/avinashpratap2003/Portfolio_ReactJs">Read More</a></button>
+                <a title="Github" href="https://github.com/avinashpratap2003/Portfolio_ReactJs"><FaGithub /></a>
+                <a title="Project Link" href="portfolio-reactjs-1.onrender.com/"><PiLinkSimpleBold /></a>
               </div>
             </div>
           </div>
@@ -52,8 +52,8 @@ const Projects = () => {
                 <img className="icon" src="stacks/Redux.svg" alt="" />
                 <img className="icon" src="stacks/Vercel.svg" alt="" />
               </div>
-              <h2 className="tint-n-orange">Breaking Copyrights</h2>
-              <p className="paragraph">
+              <h2 className="project-name">Breaking Copyrights</h2>
+              <p className="project-description">
                 A platform to download Copyrights free youtube music{" "}
               </p>
               <div className="btn-group">
@@ -79,8 +79,8 @@ const Projects = () => {
                 <img className="icon" src="stacks/Redux.svg" alt="" />
                 <img className="icon" src="stacks/Vercel.svg" alt="" />
               </div>
-              <h2 className="tint-n-orange">Isha Tattva</h2>
-              <p className="paragraph">
+              <h2 className="project-name">Isha Tattva</h2>
+              <p className="project-description">
                 A website for interior designer to showcase her works and
                 samples
               </p>
@@ -106,8 +106,8 @@ const Projects = () => {
               <!-- <img className="icon" src="stacks/Redux.svg" alt=""> --> */}
                 <img className="icon" src="stacks/Vercel.svg" alt="" />
               </div>
-              <h2 className="tint-n-orange">Granit</h2>
-              <p className="paragraph">
+              <h2 className="project-name">Granit</h2>
+              <p className="project-description">
                 An architectural websiteto plan your building or home(Hebrew
                 Language){" "}
               </p>
